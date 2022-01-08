1c15ce676704e934763982ead5a4591cbe0fc35b
mahmudulhasan01 <mahmudulh479@gmail.com>
2022-01-09 02:40:56 +0600

    navbar added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navber from "../Shared/Navber/Navber";
 import OureServices from "./OurServices/OureServices";
 import Properties from "./Pereoperties/Properties";
 
 const Home = () => {
   return (
     <div>
+      <Navber></Navber>
       <OureServices></OureServices>
       <Properties></Properties>
     </div>
