a86f82c30ef29aa0982fcae1f3827ca06b0ff9eb
mahmudulhasan01 <mahmudulh479@gmail.com>
2022-01-10 18:01:53 +0600

    Finalized Authentication and private Route

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Shop from "./Pages/Shop/Shop";
+import AuthProvider from "./Context/AuthProvider/AuthProvider";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/shop">
-            <Shop />
-          </Route>
+      <AuthProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/shop">
+              <Shop />
+            </Route>
 
-          <Route path="*">{/* <NotFound /> */}</Route>
-        </Switch>
-      </BrowserRouter>
+            <Route path="*">{/* <NotFound /> */}</Route>
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
